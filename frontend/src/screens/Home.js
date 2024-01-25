a042efdcbe30998ff90cf934e94e27a5f7eac227
MeetBoda <96529606+MeetBoda@users.noreply.github.com>
2024-01-25 08:44:09 +0530

    Added Map

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,9 +1,10 @@
-import react from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import Slider from '../components/Slider';
 import slides from '../assets/mock.json'
 import FreqAskQue from '../components/FreqAskQue';
 import Footer from '../components/Footer';
+import Map from '../components/Map';
 
 export default function Home(){
     return(
@@ -11,6 +12,7 @@ export default function Home(){
         <Navbar/>
         <Slider slides={slides}/>
         {/* <FreqAskQue/> */}
+        <Map />
         <Footer/>
         </>
     );
