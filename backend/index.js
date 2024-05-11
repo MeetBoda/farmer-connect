c605b19815e16f4197c1d831c975ef3d3bdc3716
shrutib11 <shrutibhalodia11@gmail.com>
2024-05-11 10:47:10 +0530

    Port changed

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ const UserCounter = require('./models/UserCounter')
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const mongoDB = require('./db')
+require('dotenv').config();
+const BASE_URL = process.env.BASE_URL
 
 mongoDB();
 app.use((req, res, next)=>{
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
+  res.setHeader("Access-Control-Allow-Origin", BASE_URL);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -30,4 +32,5 @@ app.use('/api', require('./Routes/ComplaintManagement'));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+
 });
